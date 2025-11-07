332e2a67d9a1b42f03c956bfb4f3e0c7a494642c
Jirateep-git-exp <jirateep.khamruang@gmail.com>
2025-11-07 13:18:51 +0700

    Add GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
+    // If you will publish to GitHub Pages as a project site, set the base
+    // to '/REPO_NAME/' so asset URLs are generated correctly.
+    base: '/jirateep-portfolio/',
     plugins: [vue()],
 });
